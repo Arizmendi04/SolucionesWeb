8a16f33f5bb36a29134e02805029dd9a2998d76c
JesusArizmendi <arizmendimj04@gmail.com>
2024-11-07 21:19:32 -0600

    cambios yatiziry <3

--- Static/view/admin/ViewGestionEmp.php
+++ Static/view/admin/ViewGestionEmp.php
@@ -6,6 +6,7 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD de Empleados</title>
