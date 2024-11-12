f04c102d3cb9171b860b53c9ab81ccdc13b3d738
Yat <amjo220898@upemor.edu.mx>
2024-11-12 11:17:10 -0600

    reporte ventas Mensuales

---
+++ Static/controller/js/ReportesConsultas.js
@@ -0,0 +1,72 @@
+// Mostrar formulario de clientes
+document.getElementById('descargar-clientes').addEventListener('click', function(event) {
+    document.getElementById('clientes-container').style.display = 'block';
+    document.getElementById('productos-container').style.display = 'none';
+    document.getElementById('empleados-container').style.display = 'none';
+    document.getElementById('productos-vendidos-container').style.display = 'none';
+    document.getElementById('ventas-mensuales-container').style.display = 'none';
+    event.stopPropagation(); // Evita que el clic se propague al documento
+});
+
+// Mostrar formulario de productos
+document.getElementById('descargar-productos').addEventListener('click', function(event) {
+    document.getElementById('productos-container').style.display = 'block';
+    document.getElementById('clientes-container').style.display = 'none';
+    document.getElementById('empleados-container').style.display = 'none';
+    document.getElementById('productos-vendidos-container').style.display = 'none';
+    document.getElementById('ventas-mensuales-container').style.display = 'none';
+    event.stopPropagation(); // Evita que el clic se propague al documento
+});
+
+// Mostrar formulario de empleados
+document.getElementById('descargar-empleados').addEventListener('click', function(event) {
+    document.getElementById('empleados-container').style.display = 'block';
+    document.getElementById('clientes-container').style.display = 'none';
+    document.getElementById('productos-container').style.display = 'none';
+    document.getElementById('productos-vendidos-container').style.display = 'none';
+    document.getElementById('ventas-mensuales-container').style.display = 'none';
+    event.stopPropagation(); // Evita que el clic se propague al documento
+});
+
+// Mostrar formulario de reporte de productos vendidos
+document.getElementById('productos-vendidos').addEventListener('click', function(event) {
+    document.getElementById('productos-vendidos-container').style.display = 'block';
+    document.getElementById('clientes-container').style.display = 'none';
+    document.getElementById('productos-container').style.display = 'none';
+    document.getElementById('empleados-container').style.display = 'none';
+    document.getElementById('ventas-mensuales-container').style.display = 'none';
+    event.stopPropagation(); // Evita que el clic se propague al documento
+});
+
+//Mostrar formulario de reporte de ventas
+document.getElementById('total-ventas').addEventListener('click', function(event) {
+    document.getElementById('ventas-mensuales-container').style.display = 'block';
+    document.getElementById('clientes-container').style.display = 'none';
+    document.getElementById('productos-container').style.display = 'none';
+    document.getElementById('empleados-container').style.display = 'none';
+    document.getElementById('productos-vendidos-container').style.display = 'none';
+    event.stopPropagation(); // Evita que el clic se propague al documento
+});
+
+// Ocultar formularios al hacer clic fuera de ellos
+document.addEventListener('click', function(event) {
+    const clientesContainer = document.getElementById('clientes-container');
+    const productosContainer = document.getElementById('productos-container');
+    const empleadosContainer = document.getElementById('empleados-container');
+    const productosVendidosContainer = document.getElementById('productos-vendidos-container');
+    const ventasMensualesContainer = document.getElementById('ventas-mensuales-container');
+
+    // Verifica si el clic ocurrió fuera de los contenedores
+    if (!clientesContainer.contains(event.target) && 
+        !productosContainer.contains(event.target) && 
+        !empleadosContainer.contains(event.target) && 
+        !productosVendidosContainer.contains(event.target)&& 
+        !ventasMensualesContainer.contains(event.target)) {
+        clientesContainer.style.display = 'none';
+        productosContainer.style.display = 'none';
+        empleadosContainer.style.display = 'none';
+        productosVendidosContainer.style.display = 'none';
+        ventasMensualesContainer.style.display = 'none';
+    }
+});
+
